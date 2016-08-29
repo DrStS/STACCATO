e924f23ce330b404e6738fa97d9bd7e4300e0fec
Stefan Sicklinger <mail@sicklinger.com>
2016-08-29 21:58:20 +0200

    Added QT 5 resources support and some icons

--- src/Qt/mainWindow.cpp
+++ src/Qt/mainWindow.cpp
@@ -6,6 +6,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    setWindowIcon(QIcon(":/Qt/resources/FitAll.png"));
 }
 
 MainWindow::~MainWindow()

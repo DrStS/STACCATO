31ac80aaa6b539a6d0ef00fe165ea9afdcec6633
Stefan Sicklinger <mail@sicklinger.com>
2016-09-04 19:26:57 +0200

    Windows x64 test

---
+++ etc/runWin64.bat
@@ -0,0 +1,6 @@
+echo Setting OpenCASCADE
+call C:\software\libs\OpenCASCADE7.0.0-vc12-64\opencascade-7.0.0\env.bat vc12 win64 Release
+echo Setting QT 5.6.1
+set "PATH=C:\software\libs\QT\QT561\5.6\msvc2013_64\bin;%PATH%"
+echo Starting STACCATO ...
+"..\bin64\Release\STACCATO.exe"

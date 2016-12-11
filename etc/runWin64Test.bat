4ba67952ddc40f23bcf2d910c0872b116633002c
Stefan Sicklinger <mail@sicklinger.com>
2016-12-11 21:08:44 +0100

    moving towards OCCT 7.1

---
+++ etc/runWin64Test.bat
@@ -0,0 +1,6 @@
+echo Setting OpenCASCADE
+call C:\software\libs\OpenCASCADE7.1.0-vc12-64\opencascade-7.1.0\env.bat vc12 win64 Release
+echo Setting QT 5.6.1
+set "PATH=C:\software\libs\QT\QT561\5.6\msvc2013_64\bin;%PATH%"
+echo Starting STACCATO ...
+"..\test\basicOCCT-QT5Example\bin64\Release\STACCATO.exe"

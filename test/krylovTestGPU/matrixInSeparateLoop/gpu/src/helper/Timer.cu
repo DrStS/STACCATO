7f587c05dabff28de45e1cf05bf278e772d7baee
Jiho Yang <jiho.yang@bmw.de>
2018-08-01 14:07:26 +0200

    created separate benchmark for spliting the program into different matrix sizes
    Former-commit-id: e68ed673430454368f1af00a761a082c9d1a189f

---
+++ test/krylovTestGPU/matrixInSeparateLoop/gpu/src/helper/Timer.cu
@@ -0,0 +1,30 @@
+/*  Copyright &copy; 2017, Stefan Sicklinger, Munich
+*
+*  All rights reserved.
+*
+*  This file is part of STACCATO.
+*
+*  STACCATO is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  STACCATO is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with STACCATO.  If not, see http://www.gnu.org/licenses/.
+*/
+#include "Timer.cuh"
+
+//Instantiate the timer object
+Timer   timerTotal;
+Timer   timerSmall;
+Timer   timerMid;
+Timer   timerLarge;
+Timer	timerIteration;
+Timer	timerLoop;
+Timer	timerIO;
+Timer	timerMatrixCpy;

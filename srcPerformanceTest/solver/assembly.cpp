4eaca12f43f1ab137156e0875f9b78170ed10b21
Stefan Sicklinger <mail@scklinger.com>
2018-09-03 09:35:08 +0200

    changed some casts interface
    Former-commit-id: 867e6622ca4fab548196b493b13dcfa6c7559d30

--- srcPerformanceTest/solver/assembly.cpp
+++ srcPerformanceTest/solver/assembly.cpp
@@ -5,7 +5,7 @@
 #include "assembly.hpp"
 
 // Assembles global matrix ( A = K - f^2*M_tilde )
-void assembly::assembleGlobalMatrix(void *A, void *K, void *M, int mat_shift, int nnz, MKL_Complex16 one, double freq_square){
+void assembly::assembleGlobalMatrix(MKL_Complex16 *A, MKL_Complex16 *K, MKL_Complex16 *M, int mat_shift, int nnz, MKL_Complex16 one, double freq_square){
     cblas_zcopy(nnz, M, 1, A + mat_shift, 1);
     cblas_zdscal(nnz, freq_square, A + mat_shift, 1);
     cblas_zaxpy(nnz, &one, K, 1, A + mat_shift, 1);
